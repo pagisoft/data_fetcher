8124e511d531ca927814b0f0916852195fde81b3
pmacialek <info@pagisoft.com>
2019-02-12 14:56:20 +0100

    Added Jersey client

--- src/main/java/com/pagisoft/datafetcher/Main.java
+++ src/main/java/com/pagisoft/datafetcher/Main.java
@@ -7,9 +7,10 @@ public class Main {
 
     private static final Logger LOGGER = LogManager.getLogger(Main.class);
 
-
     public static void main(String[] args) {
         System.out.println("test123");
+
+        new JerseyClient().getToken();
 
         LOGGER.info("Done");
     }
