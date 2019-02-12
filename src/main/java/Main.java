9884acab0507eaf1edd9801e52f005f844972a8e
pmacialek <info@pagisoft.com>
2019-02-12 12:56:49 +0100

    Added logging
    Log4j

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,14 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+
 
     public static void main(String[] args) {
         System.out.println("test123");
-    }
 
+        LOGGER.info("Done");
+    }
 }
